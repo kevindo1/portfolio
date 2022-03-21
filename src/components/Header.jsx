2aa65e68cb374605c5c6aa5bbed32d02a2cf1d98
Kevin Do <kevind0501@gmail.com>
2022-03-21 16:51:30 -0700

    fixed sizing on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
             target="_blank"
           >
             <svg
-              className="w-6 h-6"
+              className="w-8 h-8"
               aria-hidden="true"
               fill="currentColor"
               viewBox="0 0 24 24"
@@ -41,7 +41,7 @@ export default function Header() {
             target="_blank"
           >
             <svg
-              className="w-6 h-6"
+              className="w-8 h-8"
               aria-hidden="true"
               fill="currentColor"
               viewBox="0 0 448 512"
