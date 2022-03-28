bec085de0ff5b0f258dbc1fb02bb377be3f54eda
Kevin Do <kevind0501@gmail.com>
2022-03-28 10:03:10 -0700

    margin profile picture, updated meta image

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ProfilePicture() {
   return (
-    <div className="flex justify-center items-center align-middle">
+    <div className="flex justify-center items-center align-middle mr-40">
       <div className="bg-profile w-96 h-96 bg-no-repeat bg-contain item-center mx-auto my-auto flex"></div>
     </div>
   );
