a5e155d657160752295a2c1c45f402803d9969cf
Kevin Do <kevind0501@gmail.com>
2022-04-20 12:31:52 -0700

    contact page under construction

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -6,7 +6,9 @@ export default function Contact() {
       className="snap-start h-screen w-screen flex flex-col overflow-hidden"
       id="about"
     >
-      <div className="bg-sky-900 w-full h-3/4"></div>
+      <div className="bg-sky-900 w-full h-3/4 text-white font-bold text-4xl flex justify-center items-center align-middle">
+        <h1>Page still under construction, check back soon!</h1>
+      </div>
       <div className="bg-road bg-center bg-cover w-full h-1/4 text-slate-200 text-2xl flex justify-center items-center align-middle">
         © Copyright Kevin Do 2022
       </div>
