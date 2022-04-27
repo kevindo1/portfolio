22cce930bbcb308856c92dbc051743e41e6c306f
Kevin Do <kevind0501@gmail.com>
2022-04-27 11:58:47 -0700

    removed a tag for arrow animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,7 @@ export default function Footer() {
   return (
     <div className="items-center justify-center align-middle flex scroll-smooth">
       <div className="w-20">
-        <a href="#about" className="scroll-smooth">
-          <div className="bg-arrow h-20 w-20 bg-no-repeat bg-center bg-contain animate-bounce"></div>
-        </a>
+        <div className="bg-arrow h-20 w-20 bg-no-repeat bg-center bg-contain animate-bounce"></div>
       </div>
     </div>
   );
